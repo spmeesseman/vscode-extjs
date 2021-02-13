007f361d2a2bfd0ca2518be2a87687d285cc9ce9
Scott Meesseman <spmeesseman@gmail.com>
2021-02-13 18:59:06 -0500

    chore: prepare for multi-instance server

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -7,13 +7,13 @@ import { registerProviders } from "./providers/ProviderManager";
 import ExtjsLanguageManager from "./common/ExtjsLanguageManager";
 import ServerRequest from "./common/ServerRequest";
 
-
 let client: LanguageClient;
+const clients: Map<string, LanguageClient> = new Map();
 
 
 export async function activate(context: vscode.ExtensionContext)
 {
-    console.log('Congratulations, your extension "vscode-extjs" is now active!');
+    console.log("The ExtJs Language Server is now active!");
 
     registerProviders(context);
     await run(context);
@@ -28,12 +28,14 @@ export async function activate(context: vscode.ExtensionContext)
 
 async function run(context: vscode.ExtensionContext)
 {
+    const outputChannel: vscode.OutputChannel = vscode.window.createOutputChannel("vscode-extjs");
     //
-    // The server is implemented in node
+    // The server is implemented in nodevscode.
     //
     const serverModule = context.asAbsolutePath(
         path.join("dist", "server.js")
     );
+
     //
     // The debug options for the server:
     //
@@ -47,8 +49,12 @@ async function run(context: vscode.ExtensionContext)
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
     //
-    const serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc },
+    const serverOptions: ServerOptions =
+    {
+        run: {
+            module: serverModule,
+            transport: TransportKind.ipc
+        },
         debug: {
             module: serverModule,
             transport: TransportKind.ipc,
@@ -59,36 +65,109 @@ async function run(context: vscode.ExtensionContext)
     //
     // Options to control the language client
     //
-    const clientOptions: LanguageClientOptions = {
-        // Register the server for plain text documents
-        documentSelector: [{ scheme: "file", language: "javascript" }],
+    const clientOptions: LanguageClientOptions =
+    {   //
+        // Register the server for JS documents
+        //
+        documentSelector: [{ scheme: "file", language: "javascript" } ],
+        diagnosticCollectionName: "ExtJs Language Server",
+        outputChannel,
         synchronize: {
+            //
             // Notify the server about file changes to '.clientrc files contained in the workspace
             // fileEvents: vscode.workspace.createFileSystemWatcher('**/*.js'),
-        }
+        }   //
     };
+
+    // We are only interested in language mode text
+    // if (document.languageId !== "plaintext" || (document.uri.scheme !== "file" && document.uri.scheme !== "untitled")) {
+    //     return;
+    // }
+    //
+    // const uri = document.uri;
+    // // Untitled files go to a default client.
+    // if (uri.scheme === "untitled" && !client) {
+    //     const debugOptions = { execArgv: ["--nolazy", "--inspect=6010"] };
+    //     const serverOptions = {
+    //         run: { module, transport: TransportKind.ipc },
+    //         debug: { module, transport: TransportKind.ipc, options: debugOptions}
+    //     };
+    //     const clientOptions: LanguageClientOptions = {
+    //         documentSelector: [
+    //             { scheme: "untitled", language: "plaintext" }
+    //         ],
+    //         diagnosticCollectionName: "lsp-multi-server-example",
+    //         outputChannel: outputChannel
+    //     };
+    //     client = new LanguageClient("lsp-multi-server-example", "LSP Multi Server Example", serverOptions, clientOptions);
+    //     client.start();
+    //     return;
+    // }
+    // let folder = Workspace.getWorkspaceFolder(uri);
+    // // Files outside a folder can't be handled. This might depend on the language.
+    // // Single file languages like JSON might handle files outside the workspace folders.
+    // if (!folder) {
+    //     return;
+    // }
+    // // If we have nested workspace folders we only start a server on the outer most workspace folder.
+    // folder = getOuterMostWorkspaceFolder(folder);
+    //
+    // if (!clients.has(folder.uri.toString())) {
+    //     const debugOptions = { execArgv: ["--nolazy", `--inspect=${6011 + clients.size}`] };
+    //     const serverOptions = {
+    //         run: { module, transport: TransportKind.ipc },
+    //         debug: { module, transport: TransportKind.ipc, options: debugOptions}
+    //     };
+    //     const clientOptions: LanguageClientOptions = {
+    //         documentSelector: [
+    //             { scheme: "file", language: "plaintext", pattern: `${folder.uri.fsPath}/**/*` }
+    //         ],
+    //         diagnosticCollectionName: "lsp-multi-server-example",
+    //         workspaceFolder: folder,
+    //         outputChannel: outputChannel
+    //     };
+    //     const client = new LanguageClient("lsp-multi-server-example", "LSP Multi Server Example", serverOptions, clientOptions);
+    //     client.start();
+    //     clients.set(folder.uri.toString(), client);
+    // }
 
     //
     // Create the language client and start the client.
     //
     client = new LanguageClient(
         "vscode-extjs",
-        "vscode extjs Language Server",
+        "VSCode ExtJs Language Server",
         serverOptions,
         clientOptions
     );
 
     client.start();
 
+    // vscode.workspace.onDidChangeWorkspaceFolders((event) =>
+    // {
+    //     for (const folder  of event.removed)
+    //     {
+    //         const client = clients.get(folder.uri.toString());
+    //         if (client) {
+    //             clients.delete(folder.uri.toString());
+    //             client.stop();
+    //         }
+    //     }
+    // });
+
     await client.onReady();
     return client;
 }
 
 
-export function deactivate(): Thenable<void> | undefined
+export function deactivate(): Thenable<void>
 {
-    if (!client) {
-        return undefined;
+    const promises: Thenable<void>[] = [];
+    if (client) {
+        promises.push(client.stop());
     }
-    return client.stop();
+    for (const client of clients.values()) {
+        promises.push(client.stop());
+    }
+    return Promise.all(promises).then(() => undefined);
 }
