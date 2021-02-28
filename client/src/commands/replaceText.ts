299b025599ce7222c99fb0ef6459420269548f04
Scott Meesseman <spmeesseman@gmail.com>
2021-02-28 09:48:04 -0500

    chore: command label cleanup

--- client/src/commands/replaceText.ts
+++ client/src/commands/replaceText.ts
@@ -2,7 +2,7 @@
 import { commands, ExtensionContext, Range, TextEditor, TextEditorEdit } from "vscode";
 
 
-export async function replaceText(textEditor: TextEditor, edit: TextEditorEdit, text: string | undefined, range: Range | undefined)
+export function replaceText(textEditor: TextEditor, edit: TextEditorEdit, text: string | undefined, range: Range | undefined)
 {
 	if (range && text)
 	{
@@ -14,9 +14,9 @@ export async function replaceText(textEditor: TextEditor, edit: TextEditorEdit, 
 function registerReplaceTextCommand(context: ExtensionContext)
 {
 	context.subscriptions.push(
-        commands.registerTextEditorCommand("vscode-extjs:replace-text",
-										   async (textEditor: TextEditor, edit: TextEditorEdit, text: string, range: Range) =>
-										   { await replaceText(textEditor, edit, text, range); })
+        commands.registerTextEditorCommand("vscode-extjs:replaceText",
+										   (textEditor: TextEditor, edit: TextEditorEdit, text: string, range: Range) =>
+										   { replaceText(textEditor, edit, text, range); })
     );
 }
 
