f3a2815a7e594bdf57be5a695101336e2ce130a8
Scott Meesseman <spmeesseman@gmail.com>
2021-03-02 00:26:40 -0500

    tests: add create/delete document test

--- client/src/test/suite/document.test.ts
+++ client/src/test/suite/document.test.ts
@@ -1,13 +1,14 @@
 
 import * as vscode from "vscode";
-import * as assert from "assert";
-import { getDocUri, activate, toRange, sleep } from "./helper";
+import { unlinkSync, writeFileSync } from "fs";
+import { getDocUri, getNewDocUri, activate, toRange, sleep, getDocPath } from "./helper";
 
 
-suite("Hover Tests", () =>
+suite("Document Tests", () =>
 {
 
 	const docUri = getDocUri("app/shared/src/app.js");
+	const newDocPath = getDocPath("app/shared/src/app2.js");
 
 
 	suiteSetup(async () =>
@@ -16,15 +17,15 @@ suite("Hover Tests", () =>
 	});
 
 
-	test("Test edit", async () =>
+	test("Test edit document", async () =>
 	{
 		const workspaceEdit = new vscode.WorkspaceEdit(),
 		 	  config = vscode.workspace.getConfiguration(),
-			  validationDelay = config.get<boolean>("extjsLangMgr.validationDelay");
+			  validationDelay = config.get<number>("extjsLangSvr.validationDelay");
 		//
 		// Set debounce to minimum for test
 		//
-		await config.update("extjsLangMgr.validationDelay", 250); // set to minimum validation delay
+		await config.update("extjsLangSvr.validationDelay", 250); // set to minimum validation delay
 		//
 		// Write document to trigger document change events - re-indexing and validation
 		//
@@ -35,10 +36,47 @@ suite("Hover Tests", () =>
 		//
 		await sleep(2000);
 		//
+		// Use the extension's vscode-extjs:replaceText command to erase the text we just inserted
+		//
+		await vscode.commands.executeCommand("vscode-extjs:replaceText", "", toRange(95, 0, 95, 56));
+		//
+		// Wait again for validation (debounce is 250ms)
+		//
+		await sleep(2000);
+		//
 		// Reset debounce to original value b4 changed.  FOr running tests locally, this technically
 		// doesnt matter when the tests are ran in a CI
 		//
-		await vscode.workspace.getConfiguration().update("extjsLangMgr.validationDelay", validationDelay);
+		await vscode.workspace.getConfiguration().update("extjsLangSvr.validationDelay", validationDelay);
+	});
+
+
+	test("Test add new document", async () =>
+	{
+		writeFileSync(
+            newDocPath,
+			"Ext.define('VSCodeExtJS.Test',\r\n" +
+            "{\r\n" +
+            '    "prop1": "vscode-taskexplorer",\r\n' +
+            '    "config":{\r\n' +
+            '        "cfg1": "node ./node_modules/vscode/bin/test",\r\n' +
+            "    }\r\n" +
+            "}\r\n"
+        );
+		//
+		// Wait
+		//
+		await sleep(2000);
+	});
+
+
+	test("Test delete document", async () =>
+	{
+		unlinkSync(newDocPath);
+		//
+		// Wait
+		//
+		await sleep(2000);
 	});
 
 });
