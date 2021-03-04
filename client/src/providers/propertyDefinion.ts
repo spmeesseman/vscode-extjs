14f3510454a88880953f2eb1db04269112d8e71e
Scott Meesseman <spmeesseman@gmail.com>
2021-03-04 12:44:06 -0500

    style: method extraction for re-use

--- client/src/providers/propertyDefinion.ts
+++ client/src/providers/propertyDefinion.ts
@@ -13,174 +13,57 @@ class PropertyDefinitionProvider implements DefinitionProvider
 {
     provideDefinition(document: TextDocument, position: Position, token: CancellationToken): ProviderResult<Location | Location[] | LocationLink[]>
     {
-        let cmpType: ComponentType = ComponentType.None;
-        const range = document.getWordRangeAtPosition(position);
-        if (range === undefined) {
-            return;
-        }
+        log.methodStart("provide definition", 1);
 
-        const line = position.line,
-              nextLine = document.lineAt(line + 1);
-        let lineText = document.getText(new Range(new Position(line, 0), nextLine.range.start))
-                               .trim().replace(/[\s\w]+=[\s]*(new)*\s*/, ""),
-            property = document.getText(range);
+        // eslint-disable-next-line prefer-const
+        let { cmpType, property, cmpClass } = extjsLangMgr.getLineProperties(document, position, "   ");
 
-        if (property === "this")
+        if (property && cmpClass && cmpType !== undefined && cmpType !== ComponentType.None)
         {
-            // TODO - this definition
-        }
-
-        //
-        // Class string literals
-        // Match string literal class, e.g.:
-        //
-        //     Ext.create("Ext.data.Connection", {
-        //     Ext.create("Ext.panel.Panel", {
-        //     requires: [ "MyApp.common.Utilities" ]
-        //
-        if (lineText.match(new RegExp(`["']{1}[\\w.]*.${property}[\\w.]*["']{1}`)) ||
-            lineText.match(new RegExp(`["']{1}[\\w.]*${property}.[\\w.]*["']{1}`)))
-        {
-            cmpType = ComponentType.Class;
-            //
-            // Strip off everything outside the quotes to get our full class name, i.e.
-            //
-            //     MyApp.common.Utilities
-            //
-            lineText = lineText.replace(/^[^"']*["']{1}/, "").replace(/["']{1}[\w\W]*$/, "");
-            //
-            // Set the property to the last piece of the class name.  We want the effect that clicking
-            // anywhere within the string references th  entore component class, not just the "part" that
-            // gets looked at when doing a goto def for a non-quoted class variable/path
-            //
-            const strParts = lineText.split(".");
-            property = strParts[strParts.length - 1];
-        }
-        //
-        // Methods
-        // Match function/method signature type, e.g.
-        //
-        //     testFn();
-        //
-        else if (lineText.match(new RegExp(`${property}\\s*\\([ \\W\\w\\{]*\\)\\s*;\\s*$`)))
-        {
-            cmpType = ComponentType.Method;
-        }
-        //
-        // Properties / configs
-        //
-        else if (lineText.match(new RegExp(`.${property}\\s*[;\\)]+\\s*$`)))
-        {
-            cmpType = ComponentType.Property;
-        }
-        //
-        // Classes (non string literal)
-        //
-        else if (lineText.match(new RegExp(`(.|^\\s*)${property}.[\\W\\w]*$`)))
-        {
-            cmpType = ComponentType.Class;
-        }
-
-        if (cmpType !== ComponentType.None)
-        {
-            let cmpClass: string | undefined;
-            const thisPath = window.activeTextEditor?.document?.uri.fsPath;
-
-            log.write("provide definition", 1);
-            log.value("   property", property, 2);
-            log.value("   component type", cmpType, 2);
-
-            if (cmpType === ComponentType.Class)
+            log.value("   component class", cmpClass, 2);
+            const fsPath = extjsLangMgr.getFilePath(cmpClass);
+            if (fsPath)
             {
-                cmpClass = lineText.substring(0, lineText.indexOf(property) + property.length);
-            }
-            else
-            {
-                cmpClass = extjsLangMgr.getComponentClass(property, cmpType, lineText, thisPath);
-                if (!cmpClass)
-                {   //
-                    // If this is a method, check for getter/setter for a config property...
-                    //
-                    if (cmpType === ComponentType.Method && (property.startsWith("get") || property.startsWith("set")))
-                    {
-                        log.write("   method not found, look for getter/setter config", 2);
-                        property = utils.lowerCaseFirstChar(property.substring(3));
+                let start = new Position(0, 0),
+                    end = new Position(0, 0);
+                let pObject = cmpType === ComponentType.Method ? extjsLangMgr.getMethod(cmpClass, property) :
+                                            (cmpType === ComponentType.Config ? extjsLangMgr.getConfig(cmpClass, property) :
+                                                                                extjsLangMgr.getProperty(cmpClass, property));
+                if (cmpType === ComponentType.Method && (property.startsWith("get") || property.startsWith("set")))
+                {
+                    const cProperty = utils.lowerCaseFirstChar(property.substring(3)),
+                            cObject = cProperty ? extjsLangMgr.getConfig(cmpClass, cProperty) : undefined;
+                    if (cObject) {
                         cmpType = ComponentType.Config;
+                        pObject = cObject;
+                        property = cProperty;
                         log.value("      config name", property, 2);
-                        cmpClass = extjsLangMgr.getComponentClass(property, cmpType, lineText, thisPath);
-                    }
-                    //
-                    // If this is a property, check for a config property...
-                    //
-                    else if (cmpType === ComponentType.Property)
-                    {
-                        log.write("   property not found, look for config", 2);
-                        cmpType = ComponentType.Config;
-                        cmpClass = extjsLangMgr.getComponentClass(property, cmpType, lineText, thisPath);
                     }
                 }
-                else
+                if (pObject)
                 {
-                    if (cmpType === ComponentType.Property)
-                    {
-                        const cfgCls = extjsLangMgr.getComponentClass(property, ComponentType.Config);
-                        if (cfgCls)
-                        {
-                            log.write("   look for config", 2);
-                            cmpType = ComponentType.Config;
-                            cmpClass = cfgCls;
-                        }
-                    }
+                    log.write("   setting position", 2);
+                    log.value("      start line", pObject.start?.line, 3);
+                    log.value("      end line", pObject.end?.line, 3);
+                    start = toVscodePosition(pObject.start);
+                    end = toVscodePosition(pObject.end);
                 }
+                const uri = Uri.file(fsPath),
+                        range = new Range(start, end);
+                log.value("   fsPath", uri.fsPath, 2);
+                log.write("   open definition file", 1);
+                log.write("provide definition complete", 1);
+                return {
+                    uri,
+                    range
+                };
             }
-
-            if (cmpClass)
-            {
-                log.value("   component class", cmpClass, 2);
-                const fsPath = extjsLangMgr.getFilePath(cmpClass);
-                if (fsPath)
-                {
-                    let start = new Position(0, 0),
-                        end = new Position(0, 0);
-                    let pObject = cmpType === ComponentType.Method ? extjsLangMgr.getMethod(cmpClass, property) :
-                                              (cmpType === ComponentType.Config ? extjsLangMgr.getConfig(cmpClass, property) :
-                                                                                  extjsLangMgr.getProperty(cmpClass, property));
-                    if (cmpType === ComponentType.Method && (property.startsWith("get") || property.startsWith("set")))
-                    {
-                        const cProperty = utils.lowerCaseFirstChar(property.substring(3)),
-                              cObject = cProperty ? extjsLangMgr.getConfig(cmpClass, cProperty) : undefined;
-                        if (cObject) {
-                            cmpType = ComponentType.Config;
-                            pObject = cObject;
-                            property = cProperty;
-                            log.value("      config name", property, 2);
-                        }
-                    }
-                    if (pObject)
-                    {
-                        log.write("   setting position", 2);
-                        log.value("      start line", pObject.start?.line, 3);
-                        log.value("      end line", pObject.end?.line, 3);
-                        start = toVscodePosition(pObject.start);
-                        end = toVscodePosition(pObject.end);
-                    }
-                    const uri = Uri.file(fsPath),
-                          range = new Range(start, end);
-                    log.value("   fsPath", uri.fsPath, 2);
-                    log.write("   open definition file", 1);
-                    log.write("provide definition complete", 1);
-                    return {
-                        uri,
-                        range
-                    };
-                }
-                else {
-                    log.write("   fs path not found", 1);
-                }
+            else {
+                log.write("   fs path not found", 1);
             }
-
-            log.write("provide definition complete", 1);
         }
+
+        log.methodDone("provide definition complete", 1);
     }
 }
 
