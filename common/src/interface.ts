ada5a1ab334d2b15152ccf0c24852fee30433b7c
Scott Meesseman <spmeesseman@gmail.com>
2021-03-05 11:10:46 -0500

    style(common): cleanup - move enums to interface module

--- common/src/interface.ts
+++ common/src/interface.ts
@@ -1,4 +1,15 @@
 
+export enum ComponentType
+{
+    None,
+    Config = 1 << 0,
+    Method = 1 << 1,
+    Property = 1 << 2,
+    Widget = 1 << 3,
+    Class = 1 << 4
+}
+
+
 export enum DeclarationType
 {
     const,
