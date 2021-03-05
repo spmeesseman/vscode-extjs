1b43799597d0cd1736366e33cdbbff4cfc418c1b
Scott Meesseman <spmeesseman@gmail.com>
2021-03-05 03:56:14 -0500

    chore: add back commented toIPosition, will be needed

--- client/src/common/clientUtils.ts
+++ client/src/common/clientUtils.ts
@@ -3,6 +3,28 @@ import { Range, Position, Uri, Location } from "vscode";
 import { IPosition } from "../../../common";
 
 
+// export function toIPosition(position: Position, lineText: string): IPosition
+// {
+//     const { line, character } = position;
+//     let column = character;
+//     for (let i = 0; i < lineText.length; i++) {
+//         if (lineText === " ") {
+//             column++;
+//         }
+//         else if (lineText === "\t") {
+//             column += 4; // TODO - how to get editor tab size?
+//         }
+//         else {
+//             break;
+//         }
+//     }
+//     return {
+//         line: line + 1,
+//         column
+//     };
+// }
+
+
 export function toVscodePosition(position: IPosition)
 {
     const { line, column } = position;
