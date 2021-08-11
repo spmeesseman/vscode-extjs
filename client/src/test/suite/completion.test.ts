f0628955f7edb4a0122568010552f4fa381e564b
Scott Meesseman <spmeesseman@gmail.com>
2021-08-11 03:27:32 -0400

    project: woops

--- client/src/test/suite/completion.test.ts
+++ client/src/test/suite/completion.test.ts
@@ -36,6 +36,229 @@ suite("Completion Tests", () =>
 	});
 
 
+	
+
+
+	test("Inline property start", async () =>
+	{
+		//
+		// Inside function
+		//
+		await testCompletion(docUri, new vscode.Position(95, 8), "", {
+			items: [
+				{ label: "VSCodeExtJS", kind: vscode.CompletionItemKind.Class },
+				{ label: "AppUtils", kind: vscode.CompletionItemKind.Class },
+				{ label: "Utils", kind: vscode.CompletionItemKind.Class },
+				{ label: "Ext", kind: vscode.CompletionItemKind.Class }
+			]
+		});
+
+		//
+		// Inside function - beginning of line
+		//
+		await testCompletion(docUri, new vscode.Position(95, 1), "", {
+			items: [
+				{ label: "VSCodeExtJS", kind: vscode.CompletionItemKind.Class },
+				{ label: "AppUtils", kind: vscode.CompletionItemKind.Class }
+			]
+		});
+
+		//
+		// Outside function 0 results
+		//
+		await testCompletion(docUri, new vscode.Position(97, 0), "", {
+			items: []
+		});
+
+		//
+		// Inside method calle expression (parameter)
+		// Line 75
+		// VSCodeExtJS.common.PhysicianDropdown.create()
+		// Inside create() i.e. create( ... )
+		//
+		await testCompletion(docUri, new vscode.Position(74, 46), "", {
+			items: [
+				{ label: "AppUtils", kind: vscode.CompletionItemKind.Class },
+				{ label: "VSCodeExtJS", kind: vscode.CompletionItemKind.Class },
+				{ label: "Utils", kind: vscode.CompletionItemKind.Class },
+				{ label: "Ext", kind: vscode.CompletionItemKind.Class }
+			]
+		});
+		await testCompletion(docUri, new vscode.Position(74, 46), "A", {
+			items: [
+				{ label: "AppUtils", kind: vscode.CompletionItemKind.Class }
+			]
+		});
+
+		//
+		// Inside object
+		// Line 124-126
+		// const phys = Ext.create('VSCodeExtJS.common.PhysicianDropdown', {
+		//
+		// });
+		//
+		await testCompletion(docUri, new vscode.Position(124, 3), "", {
+			items: [
+				{ label: "userName UserDropdown Config", kind: vscode.CompletionItemKind.Property },
+				{ label: "readOnly UserDropdown", kind: vscode.CompletionItemKind.Property }
+			]
+		});
+		await testCompletion(docUri, new vscode.Position(124, 3), "u", {
+			items: [
+				{ label: "userName UserDropdown Config", kind: vscode.CompletionItemKind.Property }
+			]
+		});
+	});
+
+
+	test("This methods", async () =>
+	{
+		//
+		// Line 71
+		// this.*
+		//
+		await testCompletion(docUri, new vscode.Position(70, 7), ".", {
+			items: [
+				{ label: "setTest", kind: vscode.CompletionItemKind.Method },
+				{ label: "getTest", kind: vscode.CompletionItemKind.Method },
+				{ label: "test Config", kind: vscode.CompletionItemKind.Property },
+				{ label: "test2 Config", kind: vscode.CompletionItemKind.Property }
+			]
+		});
+	});
+
+
+	test("Local inherited methods", async () =>
+	{
+		//
+		// const phys = Ext.create("VSCodeExtJS.common.PhysicianDropdown"...)
+		// Line 83
+		// phys.*
+		//
+		await testCompletion(docUri, new vscode.Position(82, 7), ".", {
+			items: [
+				{ label: "save", kind: vscode.CompletionItemKind.Method },
+				{ label: "load", kind: vscode.CompletionItemKind.Method },
+				{ label: "delete", kind: vscode.CompletionItemKind.Method },
+				{ label: "getPin (deprecated)", kind: vscode.CompletionItemKind.Method }
+			]
+		});
+	});
+
+
+	test("Local class instances", async () =>
+	{
+		const incDeprecated = configuration.get<boolean>("intellisenseIncludeDeprecated"),
+			  incPrivate = configuration.get<boolean>("intellisenseIncludePrivate");
+		//
+		// const phys = Ext.create("VSCodeExtJS.common.PhysicianDropdown"...)
+		// Line 83
+		// phys.*
+		//
+		await configuration.update("intellisenseIncludeDeprecated", false);
+		await configuration.update("intellisenseIncludePrivate", false);
+		await testCompletion(docUri, new vscode.Position(82, 7), ".", {
+			items: [
+				{ label: "getPinNumber (since v1.0.0)", kind: vscode.CompletionItemKind.Method }
+			]
+		});
+		await configuration.update("intellisenseIncludeDeprecated", true);
+		await testCompletion(docUri, new vscode.Position(82, 7), ".", {
+			items: [
+				{ label: "getPinNumber (since v1.0.0)", kind: vscode.CompletionItemKind.Method },
+				{ label: "getPin (deprecated)", kind: vscode.CompletionItemKind.Method }
+			]
+		});
+		await testCompletion(docUri, new vscode.Position(82, 7), ".", {
+			items: [
+				{ label: "getPinNumberInternal", kind: vscode.CompletionItemKind.Method }
+			]
+		}, false);
+		await configuration.update("intellisenseIncludePrivate", true);
+		await testCompletion(docUri, new vscode.Position(82, 7), ".", {
+			items: [
+				{ label: "getPinNumber (since v1.0.0)", kind: vscode.CompletionItemKind.Method },
+				{ label: "getPin (deprecated)", kind: vscode.CompletionItemKind.Method },
+				// { label: "getPinNumberInternal (private)", kind: vscode.CompletionItemKind.Method },
+				{ label: "getPinNumberInternal", kind: vscode.CompletionItemKind.Method }
+			]
+		});
+		await configuration.update("intellisenseIncludeDeprecated", incDeprecated);
+		await configuration.update("intellisenseIncludePrivate", incPrivate);
+	});
+
+
+	test("Sub-classes", async () =>
+	{
+		//
+		// Line 75-76
+		// VSCodeExtJS.*
+		//
+		await testCompletion(docUri, new vscode.Position(74, 14), ".", {
+			items: [
+				{ label: "common", kind: vscode.CompletionItemKind.Class },
+				{ label: "AppUtilities", kind: vscode.CompletionItemKind.Class }
+			]
+		});
+
+		//
+		// Line 72
+		// VSCodeExtJS.AppUtilities.*
+		//
+		await testCompletion(docUri, new vscode.Position(71, 27), ".", {
+			items: [
+				{ label: "alertError", kind: vscode.CompletionItemKind.Method }
+			]
+		});
+
+		//
+		// Line 75-76
+		// VSCodeExtJS.common.*
+		//
+		await testCompletion(docUri, new vscode.Position(74, 21), ".", {
+			items: [
+				{ label: "PhysicianDropdown", kind: vscode.CompletionItemKind.Class },
+				{ label: "UserDropdown", kind: vscode.CompletionItemKind.Class }
+			]
+		});
+	});
+
+
+	test("Sub-class methods", async () =>
+	{
+		//
+		// Line 72
+		// VSCodeExtJS.AppUtilities.*
+		//
+		await testCompletion(docUri, new vscode.Position(71, 27), ".", {
+			items: [
+				{ label: "alertError", kind: vscode.CompletionItemKind.Method }
+			]
+		});
+	});
+
+
+	test("Full xtype lines", async () =>
+	{
+		//
+		// Line 121-123
+		// const patient = Ext.create('VSCodeExtJS.common.PatientDropdown', {
+		//
+		// });
+		//
+		await testCompletion(docUri, new vscode.Position(121, 3), "x", {
+			items: [
+				{ label: "xtype: component", kind: vscode.CompletionItemKind.Property },
+				{ label: "xtype: grid", kind: vscode.CompletionItemKind.Property },
+				{ label: "xtype: gridpanel", kind: vscode.CompletionItemKind.Property },
+				{ label: "xtype: patientdropdown", kind: vscode.CompletionItemKind.Property },
+				{ label: "xtype: physiciandropdown", kind: vscode.CompletionItemKind.Property },
+				{ label: "xtype: userdropdown", kind: vscode.CompletionItemKind.Property }
+			]
+		});
+	});
+
+
 	test("class object configs and properties", async () =>
 	{   //
 		// Line 167
