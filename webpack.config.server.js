70bd84fc4d07873e3b71891f1ed9b9fd8dc8c708
Scott Meesseman <spmeesseman@gmail.com>
2021-08-17 05:25:00 -0400

    build: update webpack config and packages [skip ci]

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 /**
  * @type {import('webpack').Configuration}
@@ -47,6 +48,31 @@ const config =
 				loader: 'ts-loader'
 			}]
 		}]
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({ 
+				extractComments: false ,
+				parallel: true,
+				terserOptions: {
+					ecma: undefined,
+					parse: {},
+					compress: {},
+					mangle: true, // Note `mangle.properties` is `false` by default.
+					module: false,
+					// Deprecated
+					output: null,
+					format: null,
+					toplevel: false,
+					nameCache: null,
+					ie8: false,
+					keep_classnames: undefined,
+					keep_fnames: false,
+					safari10: false,
+				}
+			})
+		]
 	}
 };
 module.exports = config;
