e26ac1d80efb5584f5fd56bbb2a729e0a44d051f
Scott Meesseman <spmeesseman@gmail.com>
2021-08-31 23:16:39 -0400

    tests: add tests shell for new task view

---
+++ client/src/test/suite/taskexplorer.test.ts
@@ -0,0 +1,47 @@
+
+import * as vscode from "vscode";
+import * as assert from "assert";
+import { configuration } from "../../common/configuration";
+import { getDocUri, activate, insertDocContent, toRange, waitForValidation } from "./helper";
+
+
+suite("Task Explorer Tests", () =>
+{
+	let taskExplorerEnabled = false;
+	let validationDelay: number | undefined;
+
+
+	suiteSetup(async () =>
+    {   //
+		// Set debounce to minimum for test
+		//
+		validationDelay = configuration.get<number>("validationDelay");
+		await configuration.update("validationDelay", 250); // set to minimum validation delay
+		taskExplorerEnabled = configuration.get<boolean>("enableTaskExplorer");
+		await configuration.update("enableTaskExplorer", true);
+		await activate();
+		await waitForValidation();
+	});
+
+
+	suiteTeardown(async () =>
+    {   //
+		// Reset configuration
+		//
+		await configuration.update("validationDelay", validationDelay || 1250);
+		await configuration.update("enableTaskExplorer", taskExplorerEnabled);
+		await waitForValidation();
+		try {
+			await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+		}
+		catch {}
+		await waitForValidation();
+	});
+
+
+	test("Get build tasks", async () =>
+	{
+
+	});
+
+});
