cc743a3e8fc8b23580219fdd1f7d62a361b2e180
Scott Meesseman <spmeesseman@gmail.com>
2021-09-03 08:53:14 -0400

    project: bring addIgnore method into command class, only place used

--- client/src/commands/ignoreError.ts
+++ client/src/commands/ignoreError.ts
@@ -1,7 +1,8 @@
 
-import { commands, ExtensionContext, Range, Selection, TextDocument, Uri, window } from "vscode";
-import { utils } from "../../../common";
-import { addIgnoreError } from "../common/ignoreError";
+import { commands, ExtensionContext, Position, Range, Selection, TextDocument, window, workspace, WorkspaceEdit } from "vscode";
+import { utils, IError } from "../../../common";
+import { documentEol } from "../common/clientUtils";
+import { configuration } from "../common/configuration";
 import { extjsLangMgr } from "../extension";
 
 
@@ -14,6 +15,44 @@ async function ignoreError(code: number, document: TextDocument, range: Range | 
 			code, fsPath: document.uri.fsPath
 		}, document, range);
 		await extjsLangMgr.validateDocument(activeTextDocument, extjsLangMgr.getNamespace(activeTextDocument), "", 1);
+	}
+}
+
+
+async function addIgnoreError(error: IError, document: TextDocument, range: Range | Selection)
+{
+	if (!range && error.code)
+	{
+		const ignoreErrors = configuration.get<IError[]>("ignoreErrors", []);
+		for (const iErr of ignoreErrors) {
+			if (iErr.code === error.code) {
+				if (!iErr.fsPath || iErr.fsPath === error.fsPath) {
+					return;
+				}
+			}
+		}
+		ignoreErrors.push(error);
+		await configuration.update("ignoreErrors", ignoreErrors);
+	}
+	else
+	{
+		const workspaceEdit = new WorkspaceEdit(),
+				line = document.lineAt(range.start.line),
+				lineText = line.text.substr(0, range.start.character);
+		let pad = "",
+			firstChar = lineText.trimEnd().lastIndexOf(" ") + 1;
+
+		if (firstChar === 0) {
+			firstChar = lineText.trimEnd().lastIndexOf("\t") + 1;
+		}
+
+		for (let i = 0; i < lineText.length && (lineText[i] === " " || lineText[i] === "\t"); i++) {
+			pad += lineText[i];
+		}
+
+		const nPosition = new Position(range.start.line, firstChar);
+		workspaceEdit.insert(document.uri, nPosition, `/** vscode-extjs-ignore-${error.code} */${documentEol(document)}${pad}`);
+		workspace.applyEdit(workspaceEdit);
 	}
 }
 
